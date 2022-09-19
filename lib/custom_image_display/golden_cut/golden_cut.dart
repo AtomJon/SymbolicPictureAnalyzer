c96c431cc4101665ccf87125059d1445ac478f8b
Neutronic John <jonbruskin@gmail.com>
2022-09-19 12:19:46 +0200

    Image now scales to application size, properly drawing the overlays

--- lib/custom_image_display/golden_cut/golden_cut.dart
+++ lib/custom_image_display/golden_cut/golden_cut.dart
@@ -1,40 +1,41 @@
+import 'package:flutter/rendering.dart';
 
-  
-  import 'package:flutter/rendering.dart';
-  
 class GoldenCutDrawer {
   GoldenCutDrawer(this._paint);
-  
+
   final Paint _paint;
-  
+
   void drawGoldenCut(Canvas canvas, Size size) {
     _drawHorizontalGoldenCutLines(canvas, size);
     _drawVerticalGoldenCutLines(canvas, size);
   }
-  
-  void _drawHorizontalGoldenCutLines(Canvas canvas, Size size) {    
+
+  void _drawHorizontalGoldenCutLines(Canvas canvas, Size size) {
+    print(size);
     final squares = size.width / 8;
-    
+
     const top = .0;
     final bottom = size.height;
-    
+
     final firstLine = squares * 3;
     canvas.drawLine(Offset(firstLine, top), Offset(firstLine, bottom), _paint);
-    
+
     final secondLine = firstLine + squares * 2;
-    canvas.drawLine(Offset(secondLine, top), Offset(secondLine, bottom), _paint);
+    canvas.drawLine(
+        Offset(secondLine, top), Offset(secondLine, bottom), _paint);
   }
-  
+
   void _drawVerticalGoldenCutLines(Canvas canvas, Size size) {
     final squares = size.height / 8;
-    
+
     const left = .0;
     final right = size.width;
-    
+
     final firstLine = squares * 3;
     canvas.drawLine(Offset(left, firstLine), Offset(right, firstLine), _paint);
-    
+
     final secondLine = firstLine + squares * 2;
-    canvas.drawLine(Offset(left, secondLine), Offset(right, secondLine), _paint);
+    canvas.drawLine(
+        Offset(left, secondLine), Offset(right, secondLine), _paint);
   }
 }
